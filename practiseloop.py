a7324d43bd39d120393e6e5efb1b095331f26a68
ishwaraiengineer <ishwar.ai.engineer@gmail.com>
2026-04-24 03:47:49 +0530

    for loop basic question

---
+++ practiseloop.py
@@ -0,0 +1,36 @@
+# for i in range(1,11):
+#     print(i)
+# for i in range(20,9,-1):
+#     print(i)
+    
+# for i in range(3,30,3):
+#     print(i)
+    
+# for i in range(0,10):
+#     if i %2==0:
+#         print("even",i)
+#     else:
+#         print("odd",i)
+
+# total = 0
+# for i in range(1,101):
+#     total += i
+# print(total)
+
+# total = 1
+# for i in range(1,6):
+#     total *= i
+# print(total)
+
+# count = 0
+# for i in range(1,50):
+#     if i%7 == 0:
+#         count+=1
+#         print(i, "this is multiply by 7")
+# print("total countable number which is divisible by 7 is ", count)
+
+sum = 0
+for i in range(1,51):
+    if i %2 !=0:
+        sum += i
+print(sum)
